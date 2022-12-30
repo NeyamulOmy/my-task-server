de8b267c3682754a1df851e0f5c35c66cf174528
NeyamulOmy <neyamul.omy@gmail.com>
2022-12-30 07:58:16 +0600

    added email query

--- index.js
+++ index.js
@@ -8,25 +8,42 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bogje7w.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         const myTasksCollection = client.db('my-task').collection('my-tasks');
-
+        const completedTasksCollection = client.db('my-task').collection('completed-tasks')
 
         app.post('/mytasks', async (req, res) => {
             const mytask = req.body;
             const result = await myTasksCollection.insertOne(mytask);
             res.send(result);
         })
+        app.post('/completedtasks', async (req, res) => {
+            const completedtask = req.body;
+            const result = await completedTasksCollection.insertOne(completedtask);
+            res.send(result);
+        })
 
-        app.get('/mytasks', async (req, res) => {
+        app.get('/completedtasks', async (req, res) => {
             const query = {};
+            const completedtasks = await completedTasksCollection.find(query).toArray();
+            res.send(completedtasks);
+        })
+        app.get('/mytasks', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
             const mytasks = await myTasksCollection.find(query).toArray();
             res.send(mytasks);
+        })
+        app.delete('/mytasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await myTasksCollection.deleteOne(filter);
+            res.send(result);
         })
 
     }
